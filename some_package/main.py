75dfd9c7c941347407f61790ac9839d3f481db1e
Mak Wei Zheng <64359863+Mak-Wei-Zheng@users.noreply.github.com>
2024-06-25 15:53:06 +0800

    updating to accept some random args

--- some_package/main.py
+++ some_package/main.py
@@ -1,14 +1,21 @@
-from some_package.some_utils import print_message
 import time
+import argparse
 
 '''
-dummy script that just prints a message to terminal every 3 seconds
+Dummy script that just prints a message to terminal every 3 seconds.
 '''
 
-def main():
+def main(name, key, port):
     while True:
-        print_message()
+        print(f"Name: {name}, Key: {key}, Port: {port}")
         time.sleep(3)
 
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some arguments.')
+    parser.add_argument('--name', type=str, required=True, help='The name to be processed')
+    parser.add_argument('--key', type=str, required=True, help='The key to be processed')
+    parser.add_argument('--port', type=int, required=True, help='The port to be processed')
+
+    args = parser.parse_args()
+
+    main(args.name, args.key, args.port)
